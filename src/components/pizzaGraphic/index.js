8035caf224d28bb4c82754ea6ff00c6a5b01baf2
Lucasgit config --global user.name Lucas <lucas.thiagomiguel@hotmail.com>
2022-06-09 23:59:46 -0300

    ajustando

--- src/components/pizzaGraphic/index.js
+++ src/components/pizzaGraphic/index.js
@@ -1,20 +1,31 @@
 
 import React  from 'react';
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, ResponsiveContainer,Tooltip } from 'recharts';
 
 
 export const data02 = [
-  { name: 'A1', value: 0 },
-  { name: 'A1', value: 0 },
-  { name: 'A2', value: 0 },
-  { name: 'B1', value: 0 },
+  { name: 'total_fazenda', value: 0 },
+  { name: 'total_hectares', value: 0 },
+  { name: 'total_agricultavel', value: 0 },
+  { name: 'total_vegetacao', value: 0 },
 ];
+export const data01 = [
+  
+];
+
 
  const Pizza = (props) => {
   data02[0].value = parseInt(props.dados.total_fazenda) ;
   data02[1].value = parseInt(props.dados.total_hectares)  ;
   data02[2].value = props.dados.total_agricultavel;
   data02[3].value = props.dados.total_vegetacao;
+  var estadoo = props.dados.estado?.map((number,i) =>
+    data01.push({
+      name:  number.estado,
+      value:parseInt(number.total_estado) 
+    })
+  )
+  console.log(data01)
   
   
     return (
@@ -24,13 +35,26 @@ export const data02 = [
         <p> total_hectares:{ props.dados.total_hectares}</p>
         <p> total_agricultavel:{ props.dados.total_agricultavel}</p>
         <p> total_vegetacao:{ props.dados.total_vegetacao}</p>
+        {props.dados.estado?.map((number,i) =>
+          <p key={i}>estado:{number.estado} total_estado:{number.total_estado}</p>
+        )}
       </div>
         <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie data={data02} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-          <Pie data={data02} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-        </PieChart>
-      </ResponsiveContainer>
+          <PieChart width={400} height={400}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={data02}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            />
+            <Pie dataKey="value" data={data01} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" label />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
         
       </>
     );
